88a42b62c5148dae8fa65d6408aea91f882422ed
Tristan Cacqueray <tdecacqu@redhat.com>
2020-10-31 15:34:31 +0000

    nix-mode: mitigate nix-shell issue

--- elisp/nix.el
+++ elisp/nix.el
@@ -1,3 +1,9 @@
+;; Ensure nix-shell command does not fail: see https://github.com/NixOS/nix-mode/pull/114
+(unless (boundp 'irony-additional-clang-options)
+  (setq irony-additional-clang-options nil))
+(unless (boundp 'woman-manpath)
+  (require 'woman))
+
 (use-package nix-mode
   :config
   (add-hook 'nix-mode-hook 'format-all-mode))
