ab0b65ed13e1e0b3db79d4cbeed45f98ab4ec2d7
Tristan Cacqueray <tdecacqu@redhat.com>
2021-01-16 18:48:54 +0000

    lsp: disable fancy features

--- elisp/lsp.el
+++ elisp/lsp.el
@@ -1,4 +1,15 @@
-(use-package lsp-mode)
+(use-package lsp-mode
+  :config
+  ;; disable fancy features
+  (setq lsp-headerline-breadcrumb-enable nil)
+  (setq lsp-enable-symbol-highlighting nil)
+  (setq lsp-ui-doc-enable nil)
+  (setq lsp-ui-doc-show-with-cursor nil)
+  (setq lsp-ui-doc-show-with-mouse nil)
+  (setq lsp-lens-enable nil)
+  (setq lsp-ui-sideline-enable nil)
+  (setq lsp-ui-sideline-show-hover nil)
+  )
 
 (use-package lsp-ui
   :ensure t
