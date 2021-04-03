950aa6e3ebcf557c936fd9ef5752665eb0dc6313
Tristan Cacqueray <tdecacqu@redhat.com>
2021-04-03 15:03:54 +0000

    purescript: bump version

--- default.nix
+++ default.nix
@@ -286,8 +286,8 @@ let
           purescript = import (nixpkgs.fetchFromGitHub {
             owner = "justinwoo";
             repo = "easy-purescript-nix";
-            rev = "7b1c1635e16c7f12065db2f8ec049030fcc63655";
-            sha256 = "1nybcann9aiwbvj95p6wam8xyhxwaxmfnkxmgylxcw42np2lvbzr";
+            rev = "9b56211db78efc447abf26c39d6968e8b03924e0";
+            sha256 = "1xkbvcjx5qyz5z7qjampxnhpvvg5grv5ikqpjr1glrfs1lvjff49";
           }) { pkgs = nixpkgs; };
         in (with purescript; [ spago purs purty zephyr ]);
         emacsConfig = elisp "purescript";
