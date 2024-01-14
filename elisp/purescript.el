b12fee673f0497a3b7825fc8a2deafcfadfe4e96
Tristan Cacqueray <tdecacqu@redhat.com>
2024-01-14 08:06:11 -0500

    emacs: update purescript-mode config

--- elisp/purescript.el
+++ elisp/purescript.el
@@ -1,11 +1,19 @@
+;; When editing .purs file, checkout the psc- commands, like:
+;; 'psc-ide-server-start'. Then hit C-c C-b,
+;; Use: 'psc-ide-flycheck-insert-suggestion'
 (use-package purescript-mode
   :mode "\\.purs\\'"
   :config
   (add-hook 'purescript-mode-hook
-    (lambda ()
-      (format-all-mode)
-      (psc-ide-mode)
-      (company-mode)
-      (turn-on-purescript-indentation)
-)))
+            (lambda ()
+              (format-all-mode)
+              (psc-ide-mode)
+              (company-mode)
+              (flycheck-mode)
+              (flymake-mode -1)
+              (turn-on-purescript-indentation)))
+  (add-hook 'purescript-mode-hook
+            (lambda ()
+              (add-hook 'before-save-hook 'purescript-sort-imports nil t)))
+  )
 (use-package psci)
